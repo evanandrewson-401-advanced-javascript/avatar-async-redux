df24625c586f9af6710fc307ec5fcdc85470e30b
evanandrewson <evanandrewson@gmail.com>
2019-11-18 15:57:10 -0800

    changed characterActions.test.js to reflect changes for promise middleware

--- src/actions/characterActions.test.js
+++ src/actions/characterActions.test.js
@@ -1,16 +1,16 @@
-import { updateCharacters } from './characterActions';
+import { updateCharacters, UPDATE_CHARACTERS, UPDATE_CHARACTERS_LOADING, UPDATE_CHARACTERS_DONE } from './characterActions';
 jest.mock('../services/fetchCharacters', () => ({
   fetchCharacters: () => Promise.resolve([])
 }));
 
 describe('character actions', () => {
   it('updateCharacters dispatches the proper actions', () => {
-    const thunk = updateCharacters();
-    const dispatch = jest.fn();
-
-    return thunk(dispatch)
-      .then(() => {
-        expect(dispatch).toHaveBeenCalledTimes(3);
-      }); 
+    const result = updateCharacters();
+    expect(result).toEqual({
+      type: UPDATE_CHARACTERS,
+      pendingType: UPDATE_CHARACTERS_LOADING,
+      fulfilledType: UPDATE_CHARACTERS_DONE,
+      payload: Promise.resolve([])
+    });
   });
 });
