fa7bc5ffd48d55e7eee944a1a7fcc68f256cca53
evanandrewson <evanandrewson@gmail.com>
2019-11-18 15:49:00 -0800

    refactored action creator to use promise middleware

--- src/actions/characterActions.js
+++ src/actions/characterActions.js
@@ -4,20 +4,32 @@ export const UPDATE_CHARACTERS = 'UPDATE_CHARACTERS';
 export const UPDATE_CHARACTERS_LOADING = 'UPDATE_CHARACTERS_LOADING';
 export const UPDATE_CHARACTERS_DONE = 'UPDATE_CHARACTERS_DONE';
 
-export const updateCharacters = () => dispatch => {
-  dispatch({
-    type: UPDATE_CHARACTERS_LOADING
-  });
+// using PROMISE MIDDLEWARE
+export const updateCharacters = () => ({
+  type: UPDATE_CHARACTERS,
+  pendingType: UPDATE_CHARACTERS_LOADING,
+  fulfilledType: UPDATE_CHARACTERS_DONE,
+  payload: fetchCharacters()
+});
 
-  return fetchCharacters()
-    .then(characters => {
-      dispatch({
-        type: UPDATE_CHARACTERS,
-        payload: characters
-      });
-      dispatch({
-        type: UPDATE_CHARACTERS_DONE
-      });
-    });
 
-};
+
+
+// USING THUNK MIDDLEWARE:
+// export const updateCharacters = () => dispatch => {
+//   dispatch({
+//     type: UPDATE_CHARACTERS_LOADING
+//   });
+
+//   return fetchCharacters()
+//     .then(characters => {
+//       dispatch({
+//         type: UPDATE_CHARACTERS,
+//         payload: characters
+//       });
+//       dispatch({
+//         type: UPDATE_CHARACTERS_DONE
+//       });
+//     });
+
+// };
